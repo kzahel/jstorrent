4583b38c12527c50afb3c5ea58de673d0bd84c70
Kyle Graehl <kgraehl@gmail.com>
2025-12-08 09:01:52 +0100

    add packaging instructions readme and option to not inject public key

--- scripts/package-extension.sh
+++ scripts/package-extension.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+set -e
+
+# Files/directories to exclude from the package (CWS promotional images)
+EXCLUDE="images/cws/*"
+
 cd ../extension
-pnpm build
+
+# Build without injecting the public key (required for CWS uploads)
+SKIP_INJECT_KEY=1 pnpm build
+
 cd dist
-zip -r ../package.zip .
+zip -r ../package.zip . -x "$EXCLUDE"
