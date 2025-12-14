4fe6cf6366a79644eeca3857f2b73781afdc38fa
Kyle Graehl <kgraehl@gmail.com>
2025-12-14 19:37:23 +0100

    bump android io daemon version for upload for testing

--- android-io-daemon/app/build.gradle.kts
+++ android-io-daemon/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.jstorrent.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
