35d009eba0e784bfca1499e4cc8d4a4c45d55e63
Kyle Graehl <kgraehl@gmail.com>
2025-12-15 19:24:58 +0100

    fix share URL

--- packages/ui/src/components/GeneralPane.tsx
+++ packages/ui/src/components/GeneralPane.tsx
@@ -87,11 +87,13 @@ function buildTorrentInfo(torrent: Torrent): InfoGroup[] {
   })
 
   // Origin
-  const shareUrl = generateMagnet({
+  const magnetUri = generateMagnet({
     infoHash: torrent.infoHashStr,
     name: torrent.name,
     announce: torrent.announce,
   })
+  const shareBase = import.meta.env.SHARE_URL || 'https://jstorrent.com/share.html'
+  const shareUrl = `${shareBase}#magnet=${encodeURIComponent(magnetUri)}`
 
   groups.push({
     title: 'Origin',
