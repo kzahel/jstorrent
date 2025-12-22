2a8b4cfa4bbbf3f16197dc9997762ad955254b70
Kyle Graehl <kgraehl@gmail.com>
2025-12-22 18:19:16 +0100

    bump android

--- android-io-daemon/app/build.gradle.kts
+++ android-io-daemon/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.jstorrent.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
